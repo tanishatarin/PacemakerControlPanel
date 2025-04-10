7b04841702fb93044247c6f3061b24ad605cfa34
claireyy16 <81452928+claireyy16@users.noreply.github.com>
2025-04-10 17:31:38 -0400

    rotate

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import ControlPanel from './components/ControlPanel';
 const App: React.FC = () => {
 
   return (
-    <div className="min-h-screen bg-[#E5EDF8] p-6">
+    <div className="min-h-screen bg-[#E5EDF8] p-6 rotated-app">
       <ControlPanel />
     </div>
   );
